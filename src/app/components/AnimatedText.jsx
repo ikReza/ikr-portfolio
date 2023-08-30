8cc3221b485a2745495e42218d94f4ee40bbaf55
Ibrahim Kaiser <59397779+ikReza@users.noreply.github.com>
2023-08-30 21:28:41 +0600

    hero section created

---
+++ src/app/components/AnimatedText.jsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+const quote = {
+  initial: {
+    opacity: 0,
+  },
+  animate: {
+    opacity: 1,
+    transition: {
+      delay: 0.5,
+      staggerChildren: 0.08,
+    },
+  },
+};
+
+const singleWord = {
+  initial: {
+    opacity: 0,
+    y: 50,
+  },
+  animate: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+    },
+  },
+};
+
+const AnimatedText = ({ text, className = "" }) => {
+  return (
+    <div className="w-full mx-auto py-2 flex items-center justify-center text-center overflow-hidden">
+      <motion.h1
+        className={`inline-block w-full font-bold capitalize text-8xl ${className}`}
+        variants={quote}
+        initial="initial"
+        animate="animate"
+      >
+        {text.split(" ").map((word, idx) => (
+          <motion.span
+            key={word + "-" + idx}
+            className="inline-block"
+            variants={singleWord}
+          >
+            {word}&nbsp;
+          </motion.span>
+        ))}
+      </motion.h1>
+    </div>
+  );
+};
+
+export default AnimatedText;
--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,43 @@
 import Image from "next/image";
 import HomeLayout from "./components/HomeLayout";
+import AnimatedText from "./components/AnimatedText";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex items-center w-full min-h-screen mx-4">
+    <main className="flex items-center min-h-[90vh] mx-4 border border-black">
       <HomeLayout className="pt-0">
         <div className="flex items-center justify-between w-full border-2 border-blue-500">
           <div className="w-1/2 mx-auto border-2 border-green-400">
             <Image
-              src="/profile/dev-pic-2.png"
+              src="/profile/dev-pic.png"
               alt="Developer Image"
-              width={400}
+              width={520}
               height={0}
-              className="w-10/12 h-auto"
+              className="rounded-bl-[50%] mx-auto"
               priority
             />
           </div>
-          <div className="w-1/2">
-          <h1>Turning Vision into Relaity with Code and Design. Turning Vision into Relaity with Code and Design.</h1>
+          <div className="w-1/2 flex flex-col items-center self-center">
+            <AnimatedText
+              text="Turning Vision into Relaity with Code and Design."
+              className="!text-6xl text-left"
+            />
+            <p className="my-4 font-medium">
+              As a full-stack developer with expertise in Next.js and Tailwind
+              CSS, I am dedicated to bringing your ideas to life in the most
+              beautiful and functional way possible. Explore my latest projects,
+              showcasing my expertise in web development.
+            </p>
+            <div className="flex items-center self-start mt-2">
+              <Link
+                href="/"
+                className="flex items-center bg-black text-[whitesmoke] p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-[whitesmoke] hover:text-black border-2 border-solid border-transparent hover:border-black"
+              >
+                Resume
+              </Link>
+              <Link href="mailto:ikreza00@gmail.com">Contact</Link>
+            </div>
           </div>
         </div>
       </HomeLayout>
